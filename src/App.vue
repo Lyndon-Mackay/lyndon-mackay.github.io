cf891bdc5caecac2af17579c5fcba0aae90075cc
lyndon mackay <lyndonmackay@hotmail.com>
2024-06-03 17:33:55 +1000

    format

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-
-<section id="title">
-<h1>Lyndon's website</h1>
-</section>
-<main>
-<h2> A CV in website form</h2>
-<p>I am going to add a navigation bar at top with selected progamming langauges and maybe a brief of construction experience</p>
-</main>
+  <section id="title">
+    <h1>Lyndon's website</h1>
+  </section>
+  <main>
+    <h2>A CV in website form</h2>
+    <p>
+      I am going to add a navigation bar at top with selected progamming langauges and maybe a brief
+      of construction experience
+    </p>
+  </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
