0b2c7de049d89f9b7a817a5ac174149c0e0dbf30
lyndon mackay <lyndonmackay@hotmail.com>
2024-06-03 18:13:31 +1000

    Start of routing

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+
+    <h2>A CV in website form</h2>
+    <p>
+      I am going to add a navigation bar at top with selected programming langauges and maybe a brief
+      of construction experience
+    </p>
+    <p>Bachelor's degree in computer and Mathematical Sciences</p>
+</template>
