164aa0804540a1a2fee263c582a6c302617f91cb
lyndon mackay <lyndonmackay@hotmail.com>
2024-06-04 11:56:33 +1000

    Changable theme and use route list routing

--- assets/CsharpView-C1NW-v_Q.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-CtAdAfOa.js";const t={};function r(n,s){return c(),o("h2",null,"Some C# dot net projects")}const _=e(t,[["render",r]]);export{_ as default};
--- assets/DatabaseView-BAFUsU6I.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as a,c}from"./index-CtAdAfOa.js";const t={};function n(r,o){return a(),c("h2",null,"My database experience")}const _=e(t,[["render",n]]);export{_ as default};
