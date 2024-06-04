43dd9b60dc0fa4ab31e2b63f25c4cb109a3e933b
lyndon mackay <lyndonmackay@hotmail.com>
2024-06-04 17:40:44 +1000

    add colours to labels

--- assets/RustView-DIBzyI5v.js
+++
@@ -1,1 +0,0 @@
-import{d as e,c as t,n as o,u as n,o as s,e as a}from"./index-Dd94Ksfb.js";const m=e({__name:"RustView",setup(r){return(c,l)=>(s(),t("h2",{style:o(n(a)("h2"))},"A selected Rust project I have done",4))}});export{m as default};
