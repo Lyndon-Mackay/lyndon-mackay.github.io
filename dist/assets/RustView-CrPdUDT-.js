156701ceea24d1d5e5a790225ff79cda7b0e19f5
lyndon mackay <lyndonmackay@hotmail.com>
2024-06-04 12:06:19 +1000

    Capitalise Page links

---
+++ dist/assets/RustView-CrPdUDT-.js
@@ -0,0 +1,1 @@
+import{d as e,c as t,n as o,u as n,o as s,e as a}from"./index-JVbcTlyF.js";const m=e({__name:"RustView",setup(r){return(c,l)=>(s(),t("h2",{style:o(n(a)("h2"))},"A selected Rust project I have done",4))}});export{m as default};
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,13 @@ import HomeView from '../views/HomeView.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
-  { path: '/rust', name: 'rust', component: () => import('../views/RustView.vue') },
-  { path: '/csharp', name: 'csharp', component: () => import('../views/CsharpView.vue') },
-  { path: '/database', name: 'database', component: () => import('../views/DatabaseView.vue') },
-  { path: '/web', name: 'web', component: () => import('../views/WebdevelopmentView.vue') },
+  { path: '/rust', name: 'Rust', component: () => import('../views/RustView.vue') },
+  { path: '/csharp', name: 'Csharp', component: () => import('../views/CsharpView.vue') },
+  { path: '/database', name: 'Database', component: () => import('../views/DatabaseView.vue') },
+  { path: '/web', name: 'Web', component: () => import('../views/WebdevelopmentView.vue') },
   {
     path: '/construction',
-    name: 'construction',
+    name: 'Construction',
     component: () => import('../views/ConstructionView.vue')
   }
 ]
