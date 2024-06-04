46240a71bc59cd6b095bcce8a83656b527b6a80c
lyndon mackay <lyndonmackay@hotmail.com>
2024-06-04 19:47:54 +1000

    change to web history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from "vue-router";
+import { createWebHistory, createRouter } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const routes = [
@@ -35,7 +35,7 @@ const routes = [
   },
 ];
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 });
 export default router;
