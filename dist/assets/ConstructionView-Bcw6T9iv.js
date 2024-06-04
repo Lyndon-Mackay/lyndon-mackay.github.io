c00cbe7c2695821df365e425c1e204446b6f22ac
lyndon mackay <lyndonmackay@hotmail.com>
2024-06-04 17:40:44 +1000

    add colours to labels

--- dist/assets/ConstructionView-Bcw6T9iv.js
+++
@@ -1,1 +0,0 @@
-import{d as e,c as n,n as t,u as o,o as r,e as s}from"./index-Dd94Ksfb.js";const u=e({__name:"ConstructionView",setup(a){return(c,i)=>(r(),n("h2",{style:t(o(s)("h2"))},"My experience with construction",4))}});export{u as default};
