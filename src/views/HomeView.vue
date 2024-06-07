b233a5556dce13a5eac0b20d78e43ee69b7f3a15
lyndon mackay <lyndonmackay@hotmail.com>
2024-06-07 12:01:37 +1000

    Home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,28 @@ import { elementColour } from "../themes/index";
 
 <template>
   <h2 :style="elementColour('h2')">A CV in website form</h2>
-  <p :style="elementColour('p')">
-    I am going to add a navigation bar at top with selected programming
-    langauges and maybe a brief of construction experience
-  </p>
-  <p :style="elementColour('p')">
-    Bachelor's degree in Computer and Mathematical Sciences
-  </p>
+  <article>
+    <h3 :style="elementColour('h3')">
+      Education
+    </h3>
+    <p :style="elementColour('p')">
+      Bachelor's degree in Computer and Mathematical Sciences
+    </p>
+    <h3 :style="elementColour('h3')">Experience</h3>
+    <p :style="elementColour('p')">
+      An experienced programmer especially in the context of maintaining an ERP system.<br>
+      I have programmed in many programming languages including C#, F#, Java, Rust, Javascript, Haskell.<br>
+      knowledgable about business operations in a construction and warehousing context.<br>
+    </p>
+    <h3 :style="elementColour('h3')">Qualities </h3>
+
+    <p :style="elementColour('p')">
+      I am strongly driven to make systems as easy to use for the user as possible I constantly ask myself at everystep
+      in the interaction if the user is getting value from it.<br>
+      From a programming standpoint I like using data types in a way to architect code as neatly as I can my favourate
+      adage in regards to programming is "Make illegal states unrepresentable"<br>
+      Of course strict deadlines may not make this possible but I have a habit of returning to make improvements
+    </p>
+
+  </article>
 </template>
