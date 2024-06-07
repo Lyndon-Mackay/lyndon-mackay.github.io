ea84d3cbc5336aa17204c5caf92189f0a97af86d
lyndon mackay <lyndonmackay@hotmail.com>
2024-06-07 22:08:36 +1000

    Custom page header to be customised

--- assets/WebdevelopmentView-cN7HZDaZ.js
+++ assets/WebdevelopmentView-cN7HZDaZ.js
@@ -1,13 +1,14 @@
-import{d as a,m as s,c as i,a as e,n as o,u as n,F as r,o as m,e as d}from"./index-C6Kym6U6.js";const h=`### This website is written using VueJs
+import{d as a,m as s,c as i,a as e,n as o,u as n,F as r,o as d,e as m}from"./index-BSn_o1d5.js";const u=`### This website is written using VueJs
 
 As part of diversifying my skillset I learnt the vue framework, I have created this website as a playground for trying out various Vue and other web development practices
 
 Some Vue concepts this website uses
 
 *   List rendering
+*   conditional rendering
 *   Binding styles
-*   Routing
-*   State management with the user themes being saved, with Pinia
+*   Routing, 404 pages and redirect, meta fields and navigation guards
+*   State management with the user themes being saved, using Pinia
 *   Event handling
 *   Input bindings
 *   loading of markdown files and using this as content for most of my pages
@@ -27,4 +28,4 @@ My first job out of university I worked for the networking team transferring dat
 ### University as part of my degree
 
 As part of my bachelor's thesis I transferred a network analytics app to the web. additionally I have done various web assignments interacting with database using ajax along with serverside programming
-`,u=["innerHTML"],p=a({__name:"WebdevelopmentView",setup(c){const t=s(h).mkd;return(l,g)=>(m(),i(r,null,[e("h2",{style:o(n(d)("h2"))}," Web Development ",4),e("article",{id:"contents",innerHTML:n(t)},null,8,u)],64))}});export{p as default};
+`,c=["innerHTML"],p=a({__name:"WebdevelopmentView",setup(l){const t=s(u).mkd;return(g,h)=>(d(),i(r,null,[e("h2",{style:o(n(m)("h2"))}," Web Development ",4),e("article",{id:"contents",innerHTML:n(t)},null,8,c)],64))}});export{p as default};
