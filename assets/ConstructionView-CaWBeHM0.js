270672c59bcc47a32f248f109504ee566e2276af
lyndon mackay <lyndonmackay@hotmail.com>
2024-06-07 16:04:09 +1000

    More markdown pages

---
+++ assets/ConstructionView-CaWBeHM0.js
@@ -0,0 +1,12 @@
+import{d as o,m as t,c as r,a as e,n as s,u as n,F as i,o as c,e as m}from"./index-Ch_lR3vu.js";const u=`### Timber doors
+
+I have 5 years of experience primarily helping maintain an ERP system for a timber door company. I learnt quite a bit about the process of how a door is constructed having walked around the factory a few times
+
+### Aluminum windows, doors, showerscreens and wardrobes
+
+I have a couple of years experience with the construction of Aluminum building products.
+
+### ERP Systems
+
+I have maintained many areas of an ERP system, including order-entry, manufacturing bill of materials,pricing,labour systems and production
+`,l=["innerHTML"],y=o({__name:"ConstructionView",setup(d){const a=t(u).mkd;return(p,h)=>(c(),r(i,null,[e("h2",{style:s(n(m)("h2"))},"My experience with construction/ERP systems",4),e("article",{id:"contents",innerHTML:n(a)},null,8,l)],64))}});export{y as default};
