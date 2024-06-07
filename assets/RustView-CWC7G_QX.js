61468eaeb17b3cfc3aff164674ba04938db2e326
lyndon mackay <lyndonmackay@hotmail.com>
2024-06-07 22:32:24 +1000

    Removed on mount which broke theme loading fixed

--- assets/RustView-CWC7G_QX.js
+++ assets/RustView-CWC7G_QX.js
@@ -1,4 +1,4 @@
-import{d as t,m as o,c as r,a as e,n as s,u as n,F as i,o as d,e as l}from"./index-BSn_o1d5.js";const m=`### Formatter for the Abl Progress language
+import{d as t,m as o,c as r,a as e,n as s,u as n,F as i,o as d,e as l}from"./index-C3eU69l-.js";const m=`### Formatter for the Abl Progress language
 
 Currently the ABL Progress langauge does not have a formatter. I noticed that most of our large programs had incosistent indenting
 ,which made tracking branching code and procedures(functions) difficult. 
