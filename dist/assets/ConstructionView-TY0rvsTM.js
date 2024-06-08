4c605369bf9f40df3a09e1e6b2e9b3b0003b4216
lyndon mackay <lyndonmackay@hotmail.com>
2024-06-08 14:35:01 +1000

    CSS changes to stop x-overflow on mobile

--- dist/assets/ConstructionView-TY0rvsTM.js
+++ dist/assets/ConstructionView-TY0rvsTM.js
@@ -1,4 +1,4 @@
-import{d as t,m as o,c as s,a as e,n as r,u as n,F as i,o as m,e as c}from"./index-GdMxdaf1.js";const d=`### Timber doors
+import{d as t,m as o,c as s,a as e,n as r,u as n,F as i,o as m,e as c}from"./index-CNoZ-Cqy.js";const d=`### Timber doors
 
 I have 5 years of experience primarily helping maintain an ERP system for a timber door company.
 I learnt quite a bit about the process of how a door is constructed having walked around and observed the factory.
