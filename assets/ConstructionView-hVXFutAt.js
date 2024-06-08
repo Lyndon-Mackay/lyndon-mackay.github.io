28df06ce96549e9c189628abb4c42dae340b9795
lyndon mackay <lyndonmackay@hotmail.com>
2024-06-08 11:31:15 +1000

    Update page contents

---
+++ assets/ConstructionView-hVXFutAt.js
@@ -0,0 +1,17 @@
+import{d as t,m as o,c as s,a as e,n as r,u as n,F as i,o as m,e as c}from"./index-Bd5SOGR8.js";const d=`### Timber doors
+
+I have 5 years of experience primarily helping maintain an ERP system for a timber door company.
+I learnt quite a bit about the process of how a door is constructed having walked around and observed the factory.
+
+### Aluminum windows, doors, showerscreens and wardrobes
+
+After closing all the actionable timber door support tickets, I started assisstent the fenestration side of the business.
+Working on a large variaty of products and helping streamline validation for commanalities such as glass validation
+
+### ERP Systems
+
+I have maintained many areas of an ERP system, including order-entry, manufacturing bill of materials, pricing,
+labour systems and production paperwork.
+I have overhauled many systems improved how updates are performed and improved the way we undo changes by either 
+implementing transactions (not the default for that programming language) or outputting backups
+`,l=["innerHTML"],y=t({__name:"ConstructionView",setup(u){const a=o(d).mkd;return(p,h)=>(m(),s(i,null,[e("h2",{style:r(n(c)("h2"))},"My experience with construction/ERP systems",4),e("article",{id:"contents",innerHTML:n(a)},null,8,l)],64))}});export{y as default};
---
+++ assets/RegexView-BENVmZvi.js
@@ -0,0 +1,27 @@
+import{d as t,m as o,c as s,a as e,n as r,u as n,F as i,o as l,e as c}from"./index-Bd5SOGR8.js";const d=`### Scanning through user presentable data
+
+When users submit errors in their reports it can be located alongside some data in a consistent form but constantly changing in very large reports where you have a replicable source of errors a regular expression can be useful for jumping to the next error or helping confirm a solutions solves multiple errors
+
+### Duplicating code changes
+
+Sometimes lots of method calls need to be changed in a very large file or over a folder. Two major examples of this are
+
+1.  Adding logging to lots of variable assignments for the so dev team can see which code is triggered
+2.  Changing method calls to use a newly created replacement that takes different arguments. I needed to do this as logic was moved further ahead in the pipleline
+
+VScode features a preview of the replacement result so changes can all be looked at before they are executed
+
+### Find references without a language server protocol
+
+I have worked with programming languages without an LSP, so finding all uses of a function or a string value that shares the same name
+as a key word can be difficult
+
+One example is a keyword \`\`\`EQ\`\`\` being the same as a site name "EQ" I used regular expressions to find all instances of the string 
+and not the keyword, I used the regular expression \`\`\`[",]EQ[",]\`\`\` as EQ can appear in a comma seperated string "EQ,AR"
+
+### Quickly add logging to a test program.
+
+If a variable is getting an unexpected value the easiest way to find out what is happening without debugging tools is to 
+log at every instance the variable is mutated and then work from there
+
+`,g=["innerHTML"],f=t({__name:"RegexView",setup(u){const a=o(d).mkd;return(h,m)=>(l(),s(i,null,[e("h2",{style:r(n(c)("h2"))},"Regular Expressions",4),e("article",{id:"contents",innerHTML:n(a)},null,8,g)],64))}});export{f as default};
