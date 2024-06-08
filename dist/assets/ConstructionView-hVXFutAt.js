eb350522cfe72862b81819f578537226c133fd8e
lyndon mackay <lyndonmackay@hotmail.com>
2024-06-08 11:31:15 +1000

    Update page contents

--- dist/assets/ConstructionView-hVXFutAt.js
+++ dist/assets/ConstructionView-hVXFutAt.js
@@ -1,4 +1,4 @@
-### Timber doors
+import{d as t,m as o,c as s,a as e,n as r,u as n,F as i,o as m,e as c}from"./index-Bd5SOGR8.js";const d=`### Timber doors
 
 I have 5 years of experience primarily helping maintain an ERP system for a timber door company.
 I learnt quite a bit about the process of how a door is constructed having walked around and observed the factory.
@@ -14,3 +14,4 @@ I have maintained many areas of an ERP system, including order-entry, manufactur
 labour systems and production paperwork.
 I have overhauled many systems improved how updates are performed and improved the way we undo changes by either 
 implementing transactions (not the default for that programming language) or outputting backups
+`,l=["innerHTML"],y=t({__name:"ConstructionView",setup(u){const a=o(d).mkd;return(p,h)=>(m(),s(i,null,[e("h2",{style:r(n(c)("h2"))},"My experience with construction/ERP systems",4),e("article",{id:"contents",innerHTML:n(a)},null,8,l)],64))}});export{y as default};
